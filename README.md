01375a0c5cea53edb76975ffcb0b52dbfa329082
gil cano <48949318+gilskie@users.noreply.github.com>
2019-09-16 15:57:35 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Finals
+ Final project for python wherein tool will search a text and update it with tags based on the .ini configuration.
---
+++ SearchAndTagString.py
@@ -0,0 +1,5 @@
+class SearchAndTagString:
+    def __init__(self, search_text, tag_string_opening, tag_string_closing):
+        self.search_text = search_text
+        self.tag_string_opening = tag_string_opening
+        self.tag_string_closing = tag_string_closing
